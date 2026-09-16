d81118191d8774f9f4dc9c3f6da4cb161b11d43c
Wolf-Creation <chebaanewajdi91@gmail.com>
2026-09-16 08:40:58 +0100

    delete vercel.json

--- src/apps/api/src/config/cors.js
+++ src/apps/api/src/config/cors.js
@@ -1,8 +1,13 @@
 import cors from 'cors';
 import { env } from './env.js';
 
-const localOrigins = ['http://localhost:8443', 'http://localhost:8444'];
-const allowedOrigins = [...new Set([...env.clientUrls, ...(env.nodeEnv === 'production' ? [] : localOrigins)])];
+const localOrigins = [
+  'http://localhost:8443',
+  'http://localhost:8444',
+  'http://192.168.1.132:8443',
+  'http://192.168.1.132:8444',
+];
+const allowedOrigins = [...new Set([...env.clientUrls, ...localOrigins])];
 
 export const corsMiddleware = cors({
   origin: (requestOrigin, callback) => {
