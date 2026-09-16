5ac4b6fe0e4f0477fb1771763902eeb71b921567
Wolf-Creation <chebaanewajdi91@gmail.com>
2026-09-17 00:21:10 +0100

    Route admin domain to admin app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import AdminApp from './apps/admin/AdminApp'
+import WebsiteApp from './apps/website/WebsiteApp'
 import './index.css'
+
+const isAdminHost = window.location.hostname === 'admin.izli.tn' || window.location.hostname === 'www.admin.izli.tn'
+const RootApp = import.meta.env.MODE === 'admin' || import.meta.env.MODE === 'admin-render' || isAdminHost ? AdminApp : WebsiteApp
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>,
 )
